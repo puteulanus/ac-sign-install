0b5ca39191230b2b21df6477248a42d8ded5a7f9
Puteulanus <mj19941230@gmail.com>
2014-09-26 14:38:13 +0800

    修复主页一句话乱码问题

--- .openshift/tools/cron.php
+++ .openshift/tools/cron.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-type:text/html;charset=utf-8");
 // 这个Cron不会执行签到，仅作外部Cron访问用
 $str = file_get_contents('http://api.hitokoto.us/rand');
 $pattern = '/'.preg_quote('"hitokoto":"','/').'(.*?)'.preg_quote('",','/').'/i';
